fa2016b65300969d99bc3beeb01808fd2c9617bb
Mika Senghaas <mail@mikasenghaas.de>
2023-11-24 20:19:19 +0100

    feat(local) minor changes

--- local-test.sh
+++ local-test.sh
@@ -11,13 +11,16 @@ as expected.
 # Experiment Parameters
 exp_name=local-test
 device=cpu
-stop_epoch=1
-eval_split=[test]
 wandb_mode=disabled
+eval_split=[test]
+val_freq=100 # High to avoid running validation
+save_freq=100 # High to avoid saving too many checkpoints
+stop_epoch=1 # Low to avoid running for too long
+log_level=CRITICAL
 
 # Hyper-parameter grid
 methods=( "baseline" "baseline_pp" "matchingnet" "protonet" "maml" )
-datasets=( "tabula_muris" "swissprot" )
+datasets=( "swissprot" )
 
 for dataset in "${datasets[@]}"
 do
@@ -26,10 +29,13 @@ do
         python run.py \
            exp.name=$exp_name \
            exp.device=$device \
+           exp.val_freq=$val_freq \
+           exp.save_freq=$save_freq \
+           exp.log_level=$log_level \
            method=$method \
            method.stop_epoch=$stop_epoch \
            dataset=$dataset \
            eval_split=$eval_split \
-           wandb.mode=$wandb_mode 
+           wandb.mode=$wandb_mode
     done
 done
