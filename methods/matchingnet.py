fix(matchingnet) initialise softmax with dim

methods/matchingnet.py
@@ -12,8 +12,7 @@ from typing import Tuple, Union, List
 
 
 class MatchingNet(MetaTemplate):
-    def __init__(self, backbone : torch.nn.Module, n_way : int, n_support : int, **kwargs):
-
+    def __init__(self, backbone: torch.nn.Module, n_way: int, n_support: int, **kwargs):
         """
         MatchingNet implementation. TODO: add more explanation
 
@@ -33,7 +32,7 @@ class MatchingNet(MetaTemplate):
         # the weighted sum of the support set embeddings for each query embedding
         self.FCE = FullyContextualEmbedding(self.feat_dim)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
         # Define the Encode of the support set
         self.G_encoder = nn.LSTM(
@@ -43,7 +42,7 @@ class MatchingNet(MetaTemplate):
         # Define the device
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def encode_training_set(self, S : torch.Tensor, G_encoder : torch.nn.Module = None):
+    def encode_training_set(self, S: torch.Tensor, G_encoder: torch.nn.Module = None):
         """
         Encode the support set using the G_encoder (Default is LSTM)
 
@@ -59,25 +58,32 @@ class MatchingNet(MetaTemplate):
         # Define the G_encoder if not defined
         if G_encoder is None:
             G_encoder = self.G_encoder
-        
+
         # Obtain the last layer of the G_encoder
         # (the squeeze is to add/remove the batch dimension)
         out_G = G_encoder(S.unsqueeze(0))[0]
         # out_G shape = (n_way * n_support, feat_dim * 2)
         # (*2 since LSTM is bidirectional - we concat the forward and backward outputs)
-        out_G = out_G.squeeze(0) 
+        out_G = out_G.squeeze(0)
 
         # Obtain the G: keep the original features and add the results of the LSTM
         # from the forward and backward directions (think of it as a sort of residual connection)
         G = S + out_G[:, : S.size(1)] + out_G[:, S.size(1) :]
 
         # Normalize the G
-        G_norm = torch.norm(G, p=2, dim=1).unsqueeze(1).expand_as(G) # L2 norm
-        G_normalized = G.div(G_norm + 0.00001) # 0.00001 is to avoid division by zero
+        G_norm = torch.norm(G, p=2, dim=1).unsqueeze(1).expand_as(G)  # L2 norm
+        G_normalized = G.div(G_norm + 0.00001)  # 0.00001 is to avoid division by zero
 
         return G, G_normalized
 
-    def get_logprobs(self, z_query : torch.Tensor, G : torch.Tensor, G_normalized : torch.Tensor, Y_S : torch.Tensor, FCE : torch.nn.Module = None):
+    def get_logprobs(
+        self,
+        z_query: torch.Tensor,
+        G: torch.Tensor,
+        G_normalized: torch.Tensor,
+        Y_S: torch.Tensor,
+        FCE: torch.nn.Module = None,
+    ):
         """
         Get the log probabilities of the query set
 
@@ -86,8 +92,8 @@ class MatchingNet(MetaTemplate):
             G (torch.Tensor) : encoded support set of shape (n_way * n_support, feat_dim)
             G_normalized (torch.Tensor) : normalized encoded support set  of shape (n_way * n_support, feat_dim)
             Y_S (torch.Tensor) : one-hot encoding of the support set labels of shape (n_way * n_support, n_way)
-            FCE (torch.nn.Module) : fully contextual embedder 
-        
+            FCE (torch.nn.Module) : fully contextual embedder
+
         Returns:
             logprobs (torch.Tensor) : log probabilities of shape (n_way * n_query, n_way)
         """
@@ -99,15 +105,15 @@ class MatchingNet(MetaTemplate):
         # Obtain the Normalised Fully Contextual Embedding of the query set
         # Shape of F = (n_way * n_query, feat_dim) --> each query embedding is now a weighted sum of the support set embeddings
         F = FCE(z_query, G)
-        F_norm = torch.norm(F, p=2, dim=1).unsqueeze(1).expand_as(F) # L2 norm of each embedding
-        F_normalized = F.div(F_norm + 0.00001) # shape = (n_way * n_query, feat_dim)
+        F_norm = (
+            torch.norm(F, p=2, dim=1).unsqueeze(1).expand_as(F)
+        )  # L2 norm of each embedding
+        F_normalized = F.div(F_norm + 0.00001)  # shape = (n_way * n_query, feat_dim)
 
         # Obtain the scores of each class
         # First apply the dot product between the normalized query and support set embeddings: shape = (n_way * n_query, n_way * n_support)
         # Then apply the ReLU activation function and multiply by 100 to strengthen the highest probability after softmax
-        scores = (
-            self.relu(F_normalized.mm(G_normalized.transpose(0, 1))) * 100
-        )
+        scores = self.relu(F_normalized.mm(G_normalized.transpose(0, 1))) * 100
 
         # For each query embedding, obtain the importantce of each support embedding (we softmax along the rows)
         softmax = self.softmax(scores)
@@ -121,12 +127,14 @@ class MatchingNet(MetaTemplate):
 
         return logprobs
 
-    def set_forward(self, x : Union[List[torch.Tensor], torch.Tensor], is_feature : bool = False):
+    def set_forward(
+        self, x: Union[List[torch.Tensor], torch.Tensor], is_feature: bool = False
+    ):
         """
         Args:
             x (Union[List[torch.Tensor], torch.Tensor]) : input data of shape (batch_size, *)
             is_feature (bool) : if True, x is the feature vector of the input data
-        
+
         Returns:
             logprobs (torch.Tensor) : log probabilities of shape (n_way * n_query, n_way)
         """
@@ -143,24 +151,32 @@ class MatchingNet(MetaTemplate):
         G, G_normalized = self.encode_training_set(z_support)
 
         # Get the labels of the support set
-        y_s = self.get_episode_labels(self.n_support, enable_grad=False) # shape = (n_way * n_support,)
+        y_s = self.get_episode_labels(
+            self.n_support, enable_grad=False
+        )  # shape = (n_way * n_support,)
 
         # Get the one-hot encoding of the support set labels, make sure gradients is enabled
-        Y_S = Variable(one_hot(y_s, self.n_way)).to(self.device) # shape = (n_way * n_support, n_way)
+        Y_S = Variable(one_hot(y_s, self.n_way)).to(
+            self.device
+        )  # shape = (n_way * n_support, n_way)
 
         # Get the log probabilities for each class
-        logprobs = self.get_logprobs(z_query, G, G_normalized, Y_S) # shape = (n_way * n_query, n_way)
+        logprobs = self.get_logprobs(
+            z_query, G, G_normalized, Y_S
+        )  # shape = (n_way * n_query, n_way)
 
         return logprobs
-    
-    def set_forward_loss(self, x : Union[torch.Tensor, List[torch.Tensor]]) -> torch.Tensor:
+
+    def set_forward_loss(
+        self, x: Union[torch.Tensor, List[torch.Tensor]]
+    ) -> torch.Tensor:
         """Compute the loss for the current task.
 
         Args:
             x (Union[torch.Tensor, List[torch.Tensor]]): input (list of) tensor(s)
 
         Returns:
-            torch.Tensor: loss tensor 
+            torch.Tensor: loss tensor
         """
 
         # Get the query labels
@@ -181,7 +197,7 @@ class MatchingNet(MetaTemplate):
 
 
 class FullyContextualEmbedding(nn.Module):
-    def __init__(self, feat_dim : int):
+    def __init__(self, feat_dim: int):
         """
         Fully Contextual Embedding module using an LSTMCell for few-shot learning.
         The LSTMCell is used to iteratively update the query embedding with the weighted sum of the support set embeddings.
@@ -195,21 +211,21 @@ class FullyContextualEmbedding(nn.Module):
 
         # Define the parameters
         self.lstmcell = nn.LSTMCell(feat_dim * 2, feat_dim)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
         self.c_0 = Variable(torch.zeros(1, feat_dim))
         self.feat_dim = feat_dim
 
         # Define the device
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def forward(self, f : torch.Tensor, G : torch.Tensor) -> torch.Tensor:
+    def forward(self, f: torch.Tensor, G: torch.Tensor) -> torch.Tensor:
         """
         Compute the fully contextual embedding for the query set.
 
         Args:
             f (torch.Tensor): query set of shape (n_way * n_query, feat_dim)
             G (torch.Tensor): encoded support set of shape (n_way * n_support, feat_dim)
-        
+
         Returns:
             torch.Tensor: fully contextual embedding of shape (n_way * n_query, feat_dim)
         """