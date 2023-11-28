5b51d260c9a3328c56022a3c66458f6dcd2ad2da
LudekCizinsky <ludekcizinsky11@gmail.com>
2023-11-28 09:27:12 +0100

    refactor(sot): remove dependence on nway, nsupport as there is no need

--- methods/self_optimal_transport.py
+++ methods/self_optimal_transport.py
@@ -8,7 +8,7 @@ class SOT(object):
 
     def __init__(self, distance_metric: str = 'cosine', ot_reg: float = 0.1, sinkhorn_iterations: int = 10,
                  sigmoid: bool = False, mask_diag: bool = True, max_scale: bool = True,
-                 n_way: int = None, n_support: int = None, n_query: int = None, **kwargs):
+                 **kwargs):
         """
         :param distance_metric - Compute the cost matrix.
         :param ot_reg - Sinkhorn entropy regularization (lambda). For few-shot classification, 0.1-0.2 works best.
@@ -16,8 +16,6 @@ class SOT(object):
         :param sigmoid - If to apply sigmoid(log_p) instead of the usual exp(log_p).
         :param mask_diag - Set to true to apply diagonal masking before and after the OT.
         :param max_scale - Re-scale the SOT values to range [0,1].
-
-        Our implementation requires the number of classes (n_way), support (n_support) and query (n_query) examples.
         """
         super().__init__()
 
@@ -30,9 +28,6 @@ class SOT(object):
         self.ot_reg = ot_reg
         self.max_scale = max_scale
         self.diagonal_val = 1e3  # value to mask self-values with
-        self.n_way = n_way
-        self.n_support = n_support
-        self.n_query = n_query
 
     def compute_cost(self, X: torch.Tensor) -> torch.Tensor:
         """
@@ -58,20 +53,6 @@ class SOT(object):
                 M.fill_diagonal_(value)
         return M
 
-    def mask_support_query(self, M:torch.Tensor,value:float):
-        """
-        Mask the support-query interaction
-        """
-        assert self.n_way is not None and self.n_support is not None and self.n_query is not None
-        if M.dim() > 2:
-            raise NotImplementedError
-            # TODO create mask
-        else:
-            raise NotImplementedError
-            # TODO create mask
-        return M
-
-
     def __call__(self, X: torch.Tensor) -> torch.Tensor:
         """
         Compute the SOT features for X
@@ -79,9 +60,6 @@ class SOT(object):
         # get masked cost matrix
         C = self.compute_cost(X=X)
         M = self.mask_diagonal(C, value=self.diagonal_val)
-
-        # mask n_query interaction with n_support
-
 
         # compute self-OT
         z_log = log_sinkhorn(M=M, reg=self.ot_reg, num_iters=self.sinkhorn_iterations)
