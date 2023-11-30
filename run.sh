311903f684ecdf0667165ad6ca6d7c50a4a6aee7
Mika Senghaas <mail@mikasenghaas.de>
2023-11-30 19:26:11 +0100

    feat(*) added run script

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+: '
+Run group of experiments.
+'
+
+# Experiment Parameters (with hyperparameter grid for method and dataset)
+group=first-results
+n_way=5
+n_shot=5
+sot=( false true ) 
+methods=( "baseline" "baseline_pp" "matchingnet" "protonet" "maml" )
+datasets=( "swissprot" "tabula_muris" )
+
+for dataset in "${datasets[@]}"
+do
+    for method in "${methods[@]}"
+    do
+        for use_sot in "${sot[@]}"
+        do
+            python run.py \
+            group=$group \
+            method=$method \
+            dataset=$dataset \
+            use_sot=$use_sot \
+            n_way=$n_way \
+            n_shot=$n_shot \
+        done
+    done
+done
