245763035b4b43c277f48ecf61c6f4269dc37620
Mika Senghaas <mail@mikasenghaas.de>
2023-12-11 16:25:41 +0100

    fix(git) resolved merge conflict

--- way-shot.sh
+++ way-shot.sh
@@ -6,7 +6,7 @@ experiment with and without SOT.
 '
 
 # Experiment Parameters (with hyperparameter grid for method and dataset)
-group=way-shot
+group=tuned-way-shot
 n_ways=( 10  8  6  4  2 )
 n_shots=( 20  15  10  5  1 )
 sot=( false true ) 
@@ -17,15 +17,20 @@ for n_way in "${n_ways[@]}"
 do
     for n_shot in "${n_shots[@]}"
     do
-        for use_sot in "${sot[@]}"
-        do
-            python run.py \
-            group=$group \
-            method=$method \
-            dataset=$dataset \
-            use_sot=$use_sot \
-            n_way=$n_way \
-            n_shot=$n_shot
-        done
+          python run.py \
+          group=$group \
+          method=$method \
+          dataset=$dataset \
+          use_sot=false \
+          n_way=$n_way \
+          n_shot=$n_shot
+
+          python run.py -m \
+          group=$group \
+          method=$method \
+          dataset=$dataset \
+          use_sot=true \
+          n_way=$n_way \
+          n_shot=$n_shot
     done
 done
