c6919252679c05a381c83a8d372a5e33335152fb
Mika Senghaas <mail@mikasenghaas.de>
2023-12-12 09:11:09 +0100

    feat(bb) simplified implementation of distLayer

--- backbones/blocks.py
+++ backbones/blocks.py
@@ -53,40 +53,18 @@ class distLinear(nn.Module):
 
     def __init__(self, indim, outdim):
         super(distLinear, self).__init__()
+        # Initialise the linear layer
         self.L = nn.Linear(indim, outdim, bias=False)
-        self.class_wise_learnable_norm = True  # See the issue#4&8 in the github
-        if self.class_wise_learnable_norm:
-            WeightNorm.apply(
-                self.L, "weight", dim=0
-            )  # split the weight update component to direction and norm
+        WeightNorm.apply(self.L, "weight", dim=0)
 
-        if outdim <= 200:
-            self.scale_factor = 2
-            # a fixed scale factor to scale the output of cos value into a reasonably large input for softmax, for to reproduce the result of CUB with ResNet10, use 4. see the issue#31 in the github
-        else:
-            self.scale_factor = 10
-            # in omniglot, a larger scale factor is required to handle >1000 output classes.
+        self.scale_factor = 2
 
     def forward(self, x):
         x_norm = torch.norm(x, p=2, dim=1).unsqueeze(1).expand_as(x)
-        x_normalized = x.div(
-            x_norm + 0.00001
-        )  # divides by norm of row vectors of x, handles 0 norm cases
-        if not self.class_wise_learnable_norm:
-            L_norm = (
-                torch.norm(
-                    self.L.weight.data, p=2, dim=1
-                )  # compute the norm of each row vector of L.weight.data
-                .unsqueeze(1)
-                .expand_as(self.L.weight.data)
-            )
-            self.L.weight.data = self.L.weight.data.div(
-                L_norm + 0.00001
-            )  # normalize the row vectors of L.weight.data same as with x_normalized
-        cos_dist = self.L(
-            x_normalized
-        )  # matrix product by forward function, but when using WeightNorm, this also multiply the cosine distance by a class-wise learnable norm, see the issue#4&8 in the github
-        scores = self.scale_factor * (cos_dist)
+        x_normalized = x.div(x_norm + 0.00001)
+
+        cos_dist = self.L(x_normalized)
+        scores = self.scale_factor * cos_dist
 
         return scores
 
