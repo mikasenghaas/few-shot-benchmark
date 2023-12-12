d2494a7462bf53c79aaa5ec5738c78a944795efe
Adam Barla <barlaada@fit.cvut.cz>
2023-12-12 18:40:47 +0100

    new wayshot

--- way-shot.sh
+++ way-shot.sh
@@ -6,7 +6,7 @@ experiment with and without SOT.
 '
 
 # Experiment Parameters (with hyperparameter grid for method and dataset)
-group=tuned-way-shot
+group=new-way-shot
 n_ways=( 10  8  6  4  2 )
 n_shots=( 20  15  10  5  1 )
 sot=( false true ) 
@@ -17,20 +17,15 @@ for n_way in "${n_ways[@]}"
 do
     for n_shot in "${n_shots[@]}"
     do
+        for sot in "${sot[@]}"
+        do
           python run.py \
           group=$group \
           method=$method \
           dataset=$dataset \
-          use_sot=false \
+          use_sot=$sot \
           n_way=$n_way \
           n_shot=$n_shot
-
-          python run.py -m \
-          group=$group \
-          method=$method \
-          dataset=$dataset \
-          use_sot=true \
-          n_way=$n_way \
-          n_shot=$n_shot
+        done
     done
 done
