ffffcf44995d57a057f933ad2077b685cd3ee8b1
Mika Senghaas <mail@mikasenghaas.de>
2023-12-13 17:49:02 +0100

    feat(*) lal

--- benchmark.sh
+++ benchmark.sh
@@ -5,26 +5,22 @@ Run all experiments for fixedd n-way and n-shot.
 '
 
 # Experiment Parameters (with hyperparameter grid for method and dataset)
-group=benchmark
+group=new-tuned-benchmark
 n_way=5
 n_shot=5
-sot=( false true ) 
-methods=( "matchingnet" "protonet" "maml" "baseline" "baseline_pp" )
-datasets=( "swissprot" "tabula_muris" )
+methods=( "baseline_pp" )
+datasets=( "swissprot" ) 
 
 for dataset in "${datasets[@]}"
 do
     for method in "${methods[@]}"
     do
-        for use_sot in "${sot[@]}"
-        do
-            python run.py \
-            group=$group \
-            method=$method \
-            dataset=$dataset \
-            use_sot=$use_sot \
-            n_way=$n_way \
-            n_shot=$n_shot
-        done
+          python run.py -m \
+          group=$group \
+          method=$method \
+          dataset=$dataset \
+          use_sot=true \
+          n_way=$n_way \
+          n_shot=$n_shot
     done
 done
--- way-shot.sh
+++ way-shot.sh
@@ -9,7 +9,6 @@ experiment with and without SOT.
 group=new-way-shot
 n_ways=( 10  8  6  4  2 )
 n_shots=( 20  15  10  5  1 )
-sot=( false true ) 
 method="protonet"
 dataset="tabula_muris"
 
@@ -17,15 +16,16 @@ for n_way in "${n_ways[@]}"
 do
     for n_shot in "${n_shots[@]}"
     do
-        for sot in "${sot[@]}"
-        do
-          python run.py \
-          group=$group \
-          method=$method \
-          dataset=$dataset \
-          use_sot=$sot \
-          n_way=$n_way \
-          n_shot=$n_shot
-        done
+        python run.py \
+        group=$group \
+        method=$method \
+        dataset=$dataset \
+        use_sot=true \
+        n_way=$n_way \
+        n_shot=$n_shot \
+        train.lr=0.1 \
+        dataset.backbone.feat_dim=1024 \
+        sot.ot_reg=0.01 \
+        sot.distance=cosine
     done
 done
