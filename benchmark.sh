bfd9140cfbcfde34d54f19b3331456ed72b118a4
Mika Senghaas <mail@mikasenghaas.de>
2023-12-13 17:52:47 +0100

    fix(*) conflicts

--- benchmark.sh
+++ benchmark.sh
@@ -8,19 +8,41 @@ Run all experiments for fixedd n-way and n-shot.
 group=new-tuned-benchmark
 n_way=5
 n_shot=5
-methods=( "baseline_pp" )
-datasets=( "swissprot" ) 
+methods=( "baseline" )
+datasets=( "tabula_muris" )
 
-for dataset in "${datasets[@]}"
+lrs=(0.0001)
+feat_dim=(64 512 1024)
+sot_reg=(0.1)
+distance=("cosine") 
+
+
+for lr in "${lrs[@]}"
 do
-    for method in "${methods[@]}"
+    for dim in "${feat_dim[@]}"
     do
-          python run.py -m \
-          group=$group \
-          method=$method \
-          dataset=$dataset \
-          use_sot=true \
-          n_way=$n_way \
-          n_shot=$n_shot
+        for reg in "${sot_reg[@]}"
+        do
+            for dist in "${distance[@]}"
+            do
+                for dataset in "${datasets[@]}"
+                do
+                    for method in "${methods[@]}"
+                    do
+                        python run.py \
+                        group=$group \
+                        method=$method \
+                        dataset=$dataset \
+                        use_sot=true \
+                        n_way=$n_way \
+                        n_shot=$n_shot \
+                        train.lr=$lr \
+                        dataset.backbone.feat_dim=$dim \
+                        sot.ot_reg=$reg \
+                        sot.distance=$dist
+                    done
+                done
+            done
+        done
     done
 done
